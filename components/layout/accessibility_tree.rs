42859eef0125ec3a48c8c0ee4fbde4e78a976926
Delan Azabani <dazabani@igalia.com>
2025-12-09 15:14:01 +0800

    Fix bug where we may settle for a stale a11y tree
    Signed-off-by: Delan Azabani <dazabani@igalia.com>

--- components/layout/accessibility_tree.rs
+++ components/layout/accessibility_tree.rs
@@ -6,9 +6,10 @@ use std::sync::Arc;
 use std::thread::{self, JoinHandle};
 use std::time::Duration;
 
-use crossbeam_channel::Sender;
+use crossbeam_channel::{Receiver, RecvError, SendError, Sender, TrySendError};
 use log::info;
 use malloc_size_of_derive::MallocSizeOf;
+use parking_lot::RwLock;
 use webrender_api::crossbeam_channel;
 
 use crate::FragmentTree;
@@ -17,17 +18,18 @@ use crate::FragmentTree;
 pub(crate) struct AccessibilityTree {}
 
 pub(crate) struct AccessibilityThread {
-    sender: Sender<Arc<FragmentTree>>,
+    input_channel: Arc<DebouncingChannel>,
+    // TODO: don’t leak the thread
     join_handle: JoinHandle<()>,
 }
 
 impl AccessibilityThread {
     pub(crate) fn new() -> Self {
-        let (sender, receiver) = crossbeam_channel::bounded(0);
+        let input_channel = Arc::new(DebouncingChannel::new());
         Self {
-            sender,
+            input_channel: input_channel.clone(),
             join_handle: thread::spawn(move || {
-                while let Ok(fragment_tree) = receiver.recv() {
+                while let Ok(fragment_tree) = input_channel.recv() {
                     info!("Thinking...");
                     std::thread::sleep(Duration::from_secs(1));
                     info!(
@@ -40,8 +42,52 @@ impl AccessibilityThread {
     }
 
     pub(crate) fn send(&self, fragment_tree: Arc<FragmentTree>) {
-        // If this fails, the channel is either full (so the thread is busy)
-        // or disconnected (so the thread is gone).
-        let _ = self.sender.try_send(fragment_tree);
+        // If this fails, the channel is disconnected (so the thread is gone).
+        let _ = self.input_channel.send(fragment_tree);
+    }
+}
+
+/// A debouncing [`crossbeam_channel`].
+struct DebouncingChannel {
+    sender: Sender<Arc<FragmentTree>>,
+    receiver: Receiver<Arc<FragmentTree>>,
+    last_input: RwLock<Option<Arc<FragmentTree>>>,
+}
+
+impl DebouncingChannel {
+    fn new() -> Self {
+        let (sender, receiver) = crossbeam_channel::bounded(0);
+        Self {
+            sender,
+            receiver,
+            last_input: Default::default(),
+        }
+    }
+    /// Send a fragment tree, or fail if the channel is disconnected.
+    ///
+    /// If the channel is not currently in `recv()`, store the value for later. If a value has
+    /// already been stored, overwrite that value.
+    fn send(&self, fragment_tree: Arc<FragmentTree>) -> Result<(), SendError<Arc<FragmentTree>>> {
+        match self.sender.try_send(fragment_tree) {
+            Ok(()) => Ok(()),
+            Err(TrySendError::Disconnected(value)) => Err(SendError(value)),
+            Err(TrySendError::Full(value)) => {
+                *self.last_input.write() = Some(value);
+                Ok(())
+            },
+        }
+    }
+    /// Receive a fragment tree, or fail if the channel is disconnected.
+    ///
+    /// If there was a value stored by [Self::send()], return that instead, without trying to
+    /// receive on the channel. Either way, fragment trees are never delivered out of order.
+    fn recv(&self) -> Result<Arc<FragmentTree>, RecvError> {
+        // TODO: try to receive on the channel, and if successful, disregard the stored value.
+        // otherwise the latency of the final fragment tree in a burst may be suboptimal.
+        if let Some(last_input) = self.last_input.write().take() {
+            Ok(last_input)
+        } else {
+            self.receiver.recv()
+        }
     }
 }
